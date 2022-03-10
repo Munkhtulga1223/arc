193a8a26ba8f189de1465c46360f8eb7d06bb5c9
Munkhtulga1223 <100835564+Munkhtulga1223@users.noreply.github.com>
2022-03-10 17:34:37 +0800

    Update khongoro's.py

--- khongoro's.py
+++ khongoro's.py
@@ -5,7 +5,7 @@ def calc_energy(img):
         [-1.0, -2.0, -1.0],
     ])
     
-    filter_du = np.stack([filter_du] * 3, axis=2)
+    filter_du = np.stack([filter_du] * 3333, axis=2)
 
     filter_dv = np.array([
         [1.0, 0.0, -1.0],
